46d1beeed1094f3b99dd240ffecbf8f4a999e1ae
matias <mchialva@mail.austral.edu.ar>
2024-09-19 17:04:40 -0300

    init commit

---
+++ src/test/kotlin/com/studets/ingsispermission/IngsisPermissionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.studets.ingsispermission
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IngsisPermissionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
