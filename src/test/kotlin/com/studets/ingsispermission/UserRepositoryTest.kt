8cec694676fd36b652ec9bbba72ca048170dafc9
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2024-10-15 13:08:14 -0300

    Arranging tests

--- src/test/kotlin/com/studets/ingsispermission/UserRepositoryTest.kt
+++ src/test/kotlin/com/studets/ingsispermission/UserRepositoryTest.kt
@@ -1,4 +1,4 @@
-package com.studets.ingsispermission.simpletests
+package com.studets.ingsispermission
 
 import com.studets.ingsispermission.entities.User
 import com.studets.ingsispermission.repositories.UserRepository
@@ -8,13 +8,9 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest() // starts app context to run tests
@@ -24,11 +20,10 @@ class UserRepositoryTest {
     @Autowired
     private lateinit var userService: UserService
 
-    @MockBean //mock of the repo
+    @MockBean // mock of the repo
     private lateinit var userRepository: UserRepository
 
     @Test
-    //@Transactional // to rollback changes
     fun `add user to db`() {
         val user = User(email = "mati@example.com", auth0Id = "123")
         val savedUser = userRepository.save(user)
@@ -41,7 +36,6 @@ class UserRepositoryTest {
     }
 
     @Test
-    //@Transactional
     fun `find by email should return user`() {
         val testUser = User(email = "test@example.com", auth0Id = "auth0|123456")
         userRepository.save(testUser)
@@ -53,7 +47,6 @@ class UserRepositoryTest {
     }
 
     @Test
-    @Transactional
     fun `find all should return all users`() {
         val testUser1 = User(email = "user1@example.com", auth0Id = "auth0|123")
         val testUser2 = User(email = "user2@example.com", auth0Id = "auth0|456")
