5b8d07b98ffffd0ea5557e22c63aadb90b3734de
matias <mchialva@mail.austral.edu.ar>
2024-10-16 11:14:48 -0300

    Audience validator.

--- src/main/kotlin/com/studets/ingsispermission/security/AudienceValidator.kt
+++ src/main/kotlin/com/studets/ingsispermission/security/AudienceValidator.kt
@@ -1,6 +1,19 @@
 package com.studets.ingsispermission.security
 
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
+import org.springframework.security.oauth2.core.OAuth2Error
+import org.springframework.security.oauth2.core.OAuth2TokenValidator
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult
+import org.springframework.security.oauth2.jwt.Jwt
 
-//class AudienceValidator(private val audience: String) : OAuth2TokenValidator<OAuth2ResourceServerProperties.Jwt> {
-//}
+class AudienceValidator(private val audience: String) : OAuth2TokenValidator<Jwt> {
+    override fun validate(jwtToken: Jwt): OAuth2TokenValidatorResult {
+        return if (jwtToken.audience.contains(audience)) {
+            OAuth2TokenValidatorResult.success()
+        } else {
+            OAuth2TokenValidatorResult.failure(
+                OAuth2Error("invalid_token", "The required audience is missing", null)
+            )
+        }
+
+    }
+}
