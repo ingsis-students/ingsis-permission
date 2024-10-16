9e8a46b77345a11129c7140f64720c9b825a65e0
matias <mchialva@mail.austral.edu.ar>
2024-10-16 11:12:17 -0300

    security dependencies.

---
+++ src/main/kotlin/com/studets/ingsispermission/security/AudienceValidator.kt
@@ -0,0 +1,6 @@
+package com.studets.ingsispermission.security
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
+
+//class AudienceValidator(private val audience: String) : OAuth2TokenValidator<OAuth2ResourceServerProperties.Jwt> {
+//}
