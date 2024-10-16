ec0e4cbc0b2ede82878f019ece2d2dd70c21eb0a
matias <mchialva@mail.austral.edu.ar>
2024-10-16 11:35:50 -0300

    security config.

--- src/main/kotlin/com/studets/ingsispermission/security/OAuth2ResourceServerSecurityConfiguration.kt
+++ src/main/kotlin/com/studets/ingsispermission/security/OAuth2ResourceServerSecurityConfiguration.kt
@@ -3,11 +3,16 @@ package com.studets.ingsispermission.security
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod.GET
-import org.springframework.http.HttpMethod.POST
+import org.springframework.http.HttpMethod.*
 import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
+import org.springframework.security.oauth2.core.OAuth2TokenValidator
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.JwtValidators
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -20,14 +25,27 @@ class OAuth2ResourceServerSecurityConfiguration(@Value("\${auth0.audience}")
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http.authorizeHttpRequests {
             it
+                .requestMatchers(GET, "/api/user").hasAuthority("SCOPE_read:snippets")
+                .requestMatchers(GET, "/api/user/{email}").hasAuthority("SCOPE_read:snippets")
                 .requestMatchers(POST, "/api/user").hasAuthority("SCOPE_read:snippets")
-                .requestMatchers(GET, "/snippets/*").hasAuthority("SCOPE_read:snippets")
-                .requestMatchers(POST, "/snippets").hasAuthority("SCOPE_write:snippets")
+                .requestMatchers(PUT, "/api/user/{email}").hasAuthority("SCOPE_read:snippets")
+                .requestMatchers(POST, "/api/user/add-snippet/{email}").hasAuthority("SCOPE_read:snippets")
+                .requestMatchers(POST, "/api/user/check-owner").hasAuthority("SCOPE_read:snippets")
                 .anyRequest().authenticated()
         }
             .oauth2ResourceServer { it.jwt(withDefaults()) }
             .cors { it.disable() }
             .csrf { it.disable() }
         return http.build()
+    }
+
+    @Bean
+    fun jwtDecoder(): JwtDecoder {
+        val jwtDecoder = NimbusJwtDecoder.withIssuerLocation(issuer).build()
+        val audienceValidator: OAuth2TokenValidator<Jwt> = AudienceValidator(audience)
+        val withIssuer: OAuth2TokenValidator<Jwt> = JwtValidators.createDefaultWithIssuer(issuer)
+        val withAudience: OAuth2TokenValidator<Jwt> = DelegatingOAuth2TokenValidator(withIssuer, audienceValidator)
+        jwtDecoder.setJwtValidator(withAudience)
+        return jwtDecoder
     }
 }
