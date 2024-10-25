81323f6bffa369617e1cec9f10f8ca84af4ce9ac
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-10-25 20:45:16 -0300

    Unused import

--- src/main/kotlin/com/studets/ingsispermission/routes/UserControllerRoutes.kt
+++ src/main/kotlin/com/studets/ingsispermission/routes/UserControllerRoutes.kt
@@ -6,7 +6,6 @@ import com.studets.ingsispermission.entities.request_types.CheckRequest
 import com.studets.ingsispermission.entities.request_types.UserSnippet
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PutMapping
